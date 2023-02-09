a7bdbc5f88084bfdee79cd3fa94f04c541270b03
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-02-09 22:21:36 +0300

    added readme

--- lab1.asm
+++ lab1.asm
@@ -4,17 +4,17 @@
 .code         ; Saying that this part belongs to code, not data
 org 100h      ; Giving number, from which labels will be counted
 
-Dungeon:		; Starting suction
+Dungeon:		    ; Starting suction
 
 mov ah, 02h 		; putch()
 
-mov dl, 0ch		; gachi char
+mov dl, 0ch		    ; gachi char
 int 21h
 
 
-mov ah, 09h		; puts()
+mov ah, 09h		    ; puts()
 mov dx, offset Msg 	; refering to FUCKING SLAVES
-int 21h			; by interupting prog, printing the message
+int 21h			    ; by interupting prog, printing the message
 
 mov ax, 4c00h		; exit(0)
 int 21h
