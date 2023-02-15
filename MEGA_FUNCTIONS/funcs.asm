7d006807c1d5fc3c6f11300a0380c1f3aa557234
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-02-15 20:32:13 +0300

    added reading for decimal numbers

--- MEGA_FUNCTIONS/funcs.asm
+++ MEGA_FUNCTIONS/funcs.asm
@@ -1,3 +1,7 @@
+x = 40
+y = 20
+
+
 
 ;------------------------------------------------
 ;	Prints string
@@ -60,7 +64,7 @@ reg2bin         proc
                 std                        ; operand which tells to decrement di each time stosw is called
 
 
-@@L1:             div dl          ; dividing the ax             
+@@L1:           div dl          ; dividing the ax             
                 add ah, 48d     ; adding to surplus the constant to get ASCII of '0' or '1' 
                 
                 mov dh, al      ; saving the result of division 
@@ -125,14 +129,14 @@ reg2hex         proc
 ;   Prints dec number on the screen 
 ;------------------------------------------------
 ;	Entry:	  AX - value to print
-;             x/y - coordinates
+;                x/y - coordinates
 ;	Exit:     None
 ;	Expects:  ES = 0b800h, x != null, y != null
 ;	Destroys: ax, dx 
 ;------------------------------------------------
-print_dec       proc
+reg2dec       proc
                 mov dl, 10d                 ; setting the divider
-                mov di, 32 + y*80 + x      ; the size of two words (reg ax) + coordinates offset
+                mov di, 32 + y*80 + x     ; the size of two words (reg ax) + coordinates offset
                 std                        ; operand which tells to decrement di each time stosw is called
 
 
@@ -216,3 +220,72 @@ printline   proc
             ret
             endp
 
+
+
+;------------------------------------------------
+; Reads the dec number from keyboard and stores it in bl
+;------------------------------------------------
+;	Entry:	  keyboard input
+;                 di - pointer to string to write into
+;	Exit:     None
+;	Expects:  none
+;	Destroys: ax, bx, cx, di
+;   Returns:  the number read will be stored in AL
+;------------------------------------------------
+
+
+inpt_dec2reg  proc
+        mov di, offset mystring     ; reading the num from keyboard
+        call getline            
+        dec di                      ; moving to the last digit of num
+
+        mov bl, 0           ; preparing ax to store the num
+        mov ch, 1           ; setting counter 
+
+@@L1:
+        mov bh, [di]        ; moving one digit of the whole num
+        sub bh, 48d          ; ascii -> num
+
+        cmp ch, 1
+        je @@skip_one
+
+
+        mov al, 1d      ; computing 10*n where n is number
+        mov cl, ch      ; how many times to pow     
+
+@@next:                 ; pow 10^ch 
+        mov ah, 10
+        mul ah          ; ---
+        dec cl          ; ---
+        cmp cl, 1       ; ---
+        jne @@next      ; ---
+
+        mul bh          ; k*10^n, k - digit with the cur rank
+
+        mov bh, al      ; storing the result
+
+@@skip_one:
+
+        add bl, bh      ; adding to ax cur digit * 10^rank
+
+        dec di     ; moving to higher bytes of number
+        inc ch     ; increasing counter
+
+        cmp di, offset mystring - 1
+        jne @@L1
+
+        mov al, bl
+
+        ;------printing----------------------------
+
+        ; mov al, bl  ; preparing to print the string
+        ; mov ah, 0   ; to zero unneded part of ax
+
+        ; mov bx, 0b800h ; to videomem
+        ; mov es, bx
+        ; xor bx, bx
+
+        ; call reg2dec
+
+        ret
+        endp
