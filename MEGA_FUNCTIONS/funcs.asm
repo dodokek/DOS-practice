443b68e10ffcd0e159d776bf9f5adaa5f2ffe4e5
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-02-16 19:49:22 +0300

    added function to draw border

--- MEGA_FUNCTIONS/funcs.asm
+++ MEGA_FUNCTIONS/funcs.asm
@@ -135,8 +135,8 @@ reg2hex         proc
 ;	Destroys: ax, dx 
 ;------------------------------------------------
 reg2dec       proc
-                mov dl, 10d                 ; setting the divider
-                mov di, 32 + y*80 + x     ; the size of two words (reg ax) + coordinates offset
+                mov dl, 10d                ; setting the divider
+                mov di, 32 + y*80 + x      ; the size of two words (reg ax) + coordinates offset
                 std                        ; operand which tells to decrement di each time stosw is called
 
 
@@ -289,3 +289,248 @@ inpt_dec2reg  proc
 
         ret
         endp
+
+
+;------------------------------------------------
+; Reads the bin number from keyboard and stores it in bl
+;------------------------------------------------
+;	Entry:	  keyboard input
+;                 di - pointer to string to write into
+;	Exit:     None
+;	Expects:  none
+;	Destroys: ax, di 
+;------------------------------------------------
+
+
+inpt_bin2reg  proc
+        mov di, offset mystring     ; reading the num from keyboard
+        call getline            
+        dec di                      ; moving to the last digit of num
+
+        mov bl, 0           ; preparing ax to store the num
+        mov ch, 1           ; setting counter 
+
+@@L1:
+        mov bh, [di]        ; moving one digit of the whole num
+        sub bh, 48d          ; ascii -> num
+
+        cmp ch, 1
+        je @@skip_one
+
+
+        mov al, 1d      ; computing 2*n where n is number
+        mov cl, ch      ; how many times to pow     
+
+@@next:                 ; pow 2^ch 
+        mov ah, 2
+        mul ah          ; ---
+        dec cl          ; ---
+        cmp cl, 1       ; ---
+        jne @@next      ; ---
+
+        mul bh          ; k*10^n, k - digit with the cur rank
+
+        mov bh, al      ; storing the result
+
+@@skip_one:
+
+        add bl, bh      ; adding to ax cur digit * 10^rank
+
+        dec di     ; moving to higher bytes of number
+        inc ch     ; increasing counter
+
+        cmp di, offset mystring - 1
+        jne @@L1
+
+        ;------printing----------------------------
+
+        ; mov al, bl  ; preparing to print the string
+        ; mov ah, 0   ; to zero unneded part of ax
+
+        ; mov bx, 0b800h ; to videomem
+        ; mov es, bx
+        ; xor bx, bx
+
+        ; call reg2dec
+
+        ret
+        endp
+
+;------------------------------------------------
+; Reads the hex number from keyboard and stores it in bl
+;------------------------------------------------
+;	Entry:	  keyboard input
+;                 di - pointer to string to write into
+;	Exit:     None
+;	Expects:  none
+;	Destroys: ax, di 
+;------------------------------------------------
+
+
+inpt_hex2reg  proc
+        mov di, offset mystring     ; reading the num from keyboard
+        call getline            
+        dec di                      ; moving to the last digit of num
+
+        mov bl, 0           ; preparing ax to store the num
+        mov ch, 1           ; setting counter 
+
+@@L1:
+        mov bh, [di]        ; moving one digit of the whole num
+        
+        cmp bh, 65          ; translating 1..9, A..F to dec
+        jge @@else_1
+                sub bh, 48d     ; adding to surplus the constant to get ASCII of 0..9 
+                jmp @@endif_1
+        @@else_1:
+                sub bh, 55d     ; handling A,B,C,D,E,F letters in surplus
+        @@endif_1:
+        
+
+        cmp ch, 1
+        je @@skip_one
+
+
+        mov al, 1d      ; computing 16*n where n is number
+        mov cl, ch      ; how many times to pow     
+
+@@next:                 ; pow 16^ch 
+        mov ah, 16
+        mul ah          ; ---
+        dec cl          ; ---
+        cmp cl, 1       ; ---
+        jne @@next      ; ---
+
+        mul bh          ; k*10^n, k - digit with the cur rank
+
+        mov bh, al      ; storing the result
+
+@@skip_one:
+
+        add bl, bh      ; adding to ax cur digit * 10^rank
+
+        dec di     ; moving to higher bytes of number
+        inc ch     ; increasing counter
+
+        cmp di, offset mystring - 1
+        jne @@L1
+
+        ;------printing----------------------------
+
+        ; mov al, bl  ; preparing to print the string
+        ; mov ah, 0   ; to zero unneded part of ax
+
+        ; mov bx, 0b800h ; to videomem
+        ; mov es, bx
+        ; xor bx, bx
+
+        ; call reg2dec
+
+        ret
+        endp
+
+
+;------------------------------------------------
+; Draws the border on the given coordinates
+;------------------------------------------------
+;	Entry:	  dx: dh - x, dl - y (top-left corner)
+;	          bx: bh - height, bl - width
+;       Exit:     None
+;	Expects:  none
+;	Destroys: ax, bx, cx, dx, di
+;       Returns:  the border
+;------------------------------------------------
+draw_border     proc
+                ; mov bx, 0b800h ; to videomem
+                ; mov es, bx
+                ; xor bx, bx
+                ;         ; params of the border
+                ; mov dh, 10d  ; x 
+                ; mov dl, 10d  ; y
+                ; mov bh, 30d  ; height
+                ; mov bl, 80d  ; width
+
+                mov ax, 1   ; calculating coordinates of top-left corner 
+
+                mov cl, dl  ; * Y
+                push dx     ; saving the dx, ignoring the possible overflow
+                mul cx
+                pop dx      ; returning value of dx
+
+                mov cx, 80d ; * 80
+                push dx     ; saving the dx, ignoring the possible overflow
+                mul cx
+                pop dx      ; returning value of dx
+
+                xor cx, cx  ; cx = 0
+                mov cl, dh  ; + X
+                add ax, cx    
+                
+                push ax     ; pushing the res to have access to it later on.
+        ; drawing top horizontal
+                mov di, ax  ; putting in di the coords of top right corner.
+                
+                xor cx, cx  ; calculating di
+                mov cl, bl
+                add di, cx  ; adding width
+                std         ; every stosw it will decrement
+@@next1:
+                mov ah, 00000101b ; giving info which char to put in videomem
+                mov al, 11d
+
+                stosw             ; putting another sym in videomem
+
+                pop ax            ; returning the original coords of top left corner
+                cmp di, ax        ; exiting the loop in case di in the top left corner
+                push ax
+                jge @@next1
+        ; ----------------------
+        ; drawing bottom horizontal
+                xor ax, ax      ; ax = 0
+                mov al, bh      ; calculating offset for bottom right corner
+                mov cx, 80d
+                push dx         ; saving dx reg from erasing
+                mul cx          ; al  = height * 80
+                pop dx
+                mov cx, ax      ; storing res in cx
+
+                pop ax          ; getting coords of top-left corn
+                push ax         ; А это на новый год
+                
+                add ax, cx      ; ax = coords of bottom left corn
+                mov dx, ax      ; storing coords of bottom left in dx
+                
+                xor cx, cx      ; cx = 0
+                mov cl, bl      ; ax += width
+                mov bx, ax      ; storing coords of bottom left in bx
+                add ax, cx      ; ax = bottom right corn
+                mov di, ax      ; storing coords of bottom right in dx
+                
+                std         ; every stosw it will decrement
+@@next2:
+                mov ah, 00000101b ; giving info which char to put in videomem
+                mov al, 11d
+
+                stosw             ; putting another sym in videomem
+
+                cmp di, dx        ; exiting the loop in case di in the bottom left corn
+                jge @@next2
+        ; ----------------------    
+        ; drawing left vertical border and, somehow right border also draws, i have zero clue why
+                mov di, bx        ; storing in di coords of bottom right corner
+
+@@next3:
+                mov byte ptr es:[di], 11d                ; gachi char to videomem
+                mov byte ptr es:[di + 1], 00000101b
+
+                sub di, 80d                     ; moving up on one line
+
+                pop ax            ; restoring coords of top left corner
+                push ax
+                cmp di, ax        ; end of cycle if di is on top left corn
+                jge @@next3
+        ;-----------------------    
+
+                pop ax
+                ret
+                endp
